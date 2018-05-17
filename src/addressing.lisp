05f460f44ed062320721e5b876797e076bc8b154
Brit Butler <brit@kingcons.io>
2018-05-17 11:58:50 -0400

    Add argument printing to the disassembler.

--- src/addressing.lisp
+++ src/addressing.lisp
@@ -31,59 +31,65 @@
            #:indirect
            #:indirect-x
            #:indirect-y
-           #:relative))
+           #:relative
+           #:get-format-string))
 
 (in-package :clones.addressing)
 
-(defmacro defaddress (name &body body)
-  `(defun ,name (cpu)
-     (declare (type cpu cpu))
-     (symbol-macrolet ((memory          (cpu-memory cpu))
-                       (program-counter (cpu-pc cpu))
-                       (accumulator     (cpu-accum cpu))
-                       (x-register      (cpu-x-reg cpu))
-                       (y-register      (cpu-y-reg cpu)))
-       ,@body)))
+(defmacro defaddress (name (&key writer) &body body)
+  `(progn
+     (defun ,name (cpu)
+       (declare (type cpu cpu))
+       (symbol-macrolet ((memory          (cpu-memory cpu))
+                         (program-counter (cpu-pc cpu))
+                         (accumulator     (cpu-accum cpu))
+                         (x-register      (cpu-x-reg cpu))
+                         (y-register      (cpu-y-reg cpu)))
+         ,@body))
+     (setf (get ',name 'writer) ,writer)))
 
-(defaddress immediate
+(defun get-format-string (mode)
+  (get (find-symbol (symbol-name mode) 'clones.addressing) 'writer))
+
+(defaddress immediate (:writer "~{#$~2,'0x~}")
   program-counter)
 
-(defaddress accumulator
+(defaddress accumulator (:writer "A")
   accumulator)
 
-(defaddress zero-page
+(defaddress zero-page (:writer "~{$~2,'0x~}")
   (fetch memory program-counter))
 
-(defaddress zero-page-x
+(defaddress zero-page-x (:writer "$~{~2,'0x~}, X")
   (let ((start (fetch memory program-counter)))
     (wrap-byte (+ start x-register))))
 
-(defaddress zero-page-y
+(defaddress zero-page-y (:writer "$~{~2,'0x~}, Y")
   (let ((start (fetch memory program-counter)))
     (wrap-byte (+ start y-register))))
 
-(defaddress absolute
+(defaddress absolute (:writer "$~{~2,'0x~}")
   (fetch-word memory program-counter))
 
-(defaddress absolute-x
+(defaddress absolute-x (:writer "$~{~2,'0x~}, X")
   (let* ((start (fetch-word memory program-counter))
          (final (wrap-word (+ start x-register))))
     (values final start)))
 
-(defaddress absolute-y
+(defaddress absolute-y (:writer "$~{~2,'0x~}, Y")
   (let* ((start (fetch-word memory program-counter))
          (final (wrap-word (+ start y-register))))
     (values final start)))
 
-(defaddress indirect
+(defaddress indirect (:writer "($~{~2,'0x~})")
   (let ((start (fetch-word memory program-counter)))
     (fetch-indirect memory start)))
 
-(defaddress indirect-x
+(defaddress indirect-x (:writer "($~{~2,'0x~}), X")
   (let ((start (wrap-byte (+ (fetch memory program-counter) x-register))))
     (fetch-indirect memory start)))
 
-(defaddress indirect-y
+(defaddress indirect-y (:writer "($~{~2,'0x~}), Y")
   (let* ((start (fetch-indirect memory (fetch memory program-counter)))
          (final (wrap-word (+ start y-register))))
     (values final start)))
@@ -92,7 +98,7 @@
 ;; Offset is a signed byte in two's complement form.
 ;; If negative, xor the offset with 255 to determine the unsigned value and jump.
 ;; If positive, add one to offset to step over the offset and jump.
-(defaddress relative
+(defaddress relative (:writer "&~{~2,'0x~}")
   (let ((offset (fetch memory program-counter)))
     (if (logbitp 7 offset)
         (wrap-word (- program-counter (logxor #xff offset)))
