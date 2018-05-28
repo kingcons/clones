cb624a0b02e4980d2743a475077b1ba2443be8c0
Brit Butler <brit@kingcons.io>
2018-05-28 18:13:26 -0400

    Sketch out display and main app functionality.

--- src/clones.lisp
+++ src/clones.lisp
@@ -1,6 +1,42 @@
 (in-package :cl-user)
+
 (defpackage clones
-  (:use :cl))
+  (:use :cl)
+  (:import-from :clones.display
+                :init-display
+                :display-frame)
+  (:import-from :clones.cpu
+                :make-cpu
+                :cpu-memory
+                :reset)
+  (:import-from :clones.instructions
+                :scanline-step)
+  (:import-from :clones.memory
+                :swap-rom))
+
 (in-package :clones)
 
-;; blah blah blah.
+(defvar *nes* (make-cpu))
+(defvar *trace* nil)
+
+(defun change-game (rom-file)
+  (swap-rom (cpu-memory *nes*) rom-file)
+  (reset *nes*))
+
+(defun tracing ()
+  (when *trace*
+    (format t *nes*)
+    (clones.disassembler:now)))
+
+(defun play ()
+  ;; KLUDGE: SCANLINE-STEP should probably be a generic function if we're using it like this.
+  (init-display)
+  (with-slots (ppu apu) (cpu-memory *nes*)
+    (loop
+      (scanline-step *nes*)
+      (tracing)
+      (let ((ppu-result (clones.ppu:scanline-step ppu)))
+        (when (getf ppu-result :vblank-nmi)
+          (nmi *nes*))
+        (when (getf ppu-result :new-frame)
+          (display-frame))))))
--- src/display.lisp
+++ src/display.lisp
@@ -1,45 +1,37 @@
 (in-package :cl-user)
 
 (defpackage :clones.display
-  (:use :cl :clones.ppu))
+  (:use :cl)
+  (:import-from :clones.ppu
+                :*framebuffer*)
+  (:import-from :static-vectors
+                :static-vector-ptr)
+  (:export #:init-display))
 
 (in-package :clones.display)
 
 (defvar *screen-width* 256)
 (defvar *screen-height* 240)
+(defvar *display* nil)
+(defvar *renderer* nil)
+(defvar *texture* nil)
 
-(defun randomize-buffer (buffer)
-  (loop for i from 0 upto (1- (length buffer))
-        do (setf (aref buffer i) (random 255))))
+(defun init-display ()
+  (sdl2:in-main-thread ()
+    (sdl2:init :everything)
+    (setf *display*  (sdl2:create-window :title "Clones"
+                                         :w *screen-width*
+                                         :h *screen-height*)
+          *renderer* (sdl2:create-renderer *display*
+                                           :flags '(:accelerated))
+          *texture*  (sdl2:create-texture renderer :bgr24 :streaming 256 240))))
 
-(defun new-frame (renderer framebuffer texture)
-  (let ((start-of-frame (get-internal-real-time)))
-    (randomize-buffer framebuffer)
-    ;; see: https://wiki.libsdl.org/SDL_UpdateTexture
-    (sdl2:update-texture texture (static-vectors:static-vector-pointer framebuffer)
-                         :rect (cffi:null-pointer) :width (* 3 *screen-width*))
-    (sdl2:render-copy renderer texture)
-    (sdl2:render-present renderer)
-    (format t "Frame drawn in ~A milliseconds~%" (- (get-internal-real-time) start-of-frame))))
-
-(defun draw (&key (width *screen-width*) (height *screen-height*))
-  (let ((size (* width height 3)))
-    (static-vectors:with-static-vector (framebuffer size :element-type '(unsigned-byte 8))
-      (sdl2:with-init (:video)
-        (sdl2:with-window (window :title "Clones Demo" :w 256 :h 240)
-          (sdl2:with-renderer (renderer window :flags '(:accelerated))
-            (let ((texture (sdl2:create-texture renderer :bgr24 :streaming 256 240)))
-              (sdl2:with-event-loop (:method :poll)
-                (:keyup
-                 (:keysym keysym)
-                 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-                   (sdl2:push-event :quit))
-                 (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-right)
-                   (new-frame renderer framebuffer texture)))
-                (:idle
-                 ()
-                 )
-                (:quit () t)))))))))
-
-(defun test ()
-  (sdl2:make-this-thread-main #'draw))
+(defun display-frame ()
+  (sdl2:in-main-thread ()
+    (let ((start-of-frame (get-internal-real-time)))
+      (sdl2:update-texture *texture* (static-vector-ptr *framebuffer*)
+                           :rect (cffi:null-pointer)
+                           :width (* 3 *screen-width*))
+      (sdl2:render-copy *renderer* *texture*)
+      (sdl2:render-present *renderer*)
+      (format t "Frame drawn in ~A milliseconds~%" (- (get-internal-real-time) start-of-frame)))))
