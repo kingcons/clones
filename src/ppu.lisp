c39b557f7569edf7e80e886edc304b3cb34e9ffd
Brit Butler <brit@kingcons.io>
2018-08-31 14:31:26 -0400

    Get basic registers in place.

--- src/ppu.lisp
+++ src/ppu.lisp
@@ -1,6 +1,34 @@
 (in-package :cl-user)
 
 (defpackage :clones.ppu
-  (:use :cl))
+  (:use :cl)
+  (:import-from :clones.util :ub8
+                             :ub16)
+  (:export #:ppu
+           #:make-ppu
+           #:ppu-control
+           #:ppu-mask
+           #:ppu-status
+           #:ppu-oam-address
+           #:ppu-data
+           #:ppu-address
+           #:ppu-coarse-x
+           #:ppu-coarse-y
+           #:ppu-fine-x
+           #:ppu-fine-y
+           #:ppu-nametable))
 
 (in-package :clones.ppu)
+
+(defstruct ppu
+  (control      0 :type ub8)  ;; 0x2000
+  (mask         0 :type ub8)  ;; 0x2001
+  (status       0 :type ub8)  ;; 0x2002
+  (oam-address  0 :type ub8)  ;; 0x2003/0x2004
+  (address      0 :type ub16) ;; 0x2006
+  (data         0 :type ub8)  ;; 0x2007
+  (coarse-x     0 :type ub8)  ;; 0x2005
+  (coarse-y     0 :type ub8)  ;; 0x2005
+  (fine-x       0 :type ub8)  ;; 0x2005
+  (fine-y       0 :type ub8)  ;; 0x2005
+  (nametable    0 :type ub8)) ;; 0x2005
--- t/ppu.lisp
+++ t/ppu.lisp
@@ -9,9 +9,24 @@
   (let ((ppu (make-ppu)))
     (is (type-of ppu) 'ppu)))
 
+(defun test-ppu-registers-init ()
+  (let ((ppu (make-ppu)))
+    (is (ppu-control ppu) 0)
+    (is (ppu-mask ppu) 0)
+    (is (ppu-status ppu) 0)
+    (is (ppu-oam-address ppu) 0)
+    (is (ppu-address ppu) 0)
+    (is (ppu-data ppu) 0)
+    (is (ppu-coarse-x ppu) 0)
+    (is (ppu-coarse-y ppu) 0)
+    (is (ppu-nametable ppu) 0)
+    (is (ppu-fine-x ppu) 0)
+    (is (ppu-fine-y ppu) 0)))
+
 (plan 1)
 
 (subtest "PPU Interface"
-  (test-ppu-construction))
+  (test-ppu-construction)
+  (test-ppu-registers-init))
 
 (finalize)
