f3e0e33639d7c9cd75cb71bca216a65502de1f6a
Brit Butler <brit@kingcons.io>
2018-08-31 11:28:21 -0400

    We're not actually using the #f read macro anymore. Remove it.

--- src/util.lisp
+++ src/util.lisp
@@ -3,7 +3,6 @@
 (defpackage :clones.util
   (:use :cl)
   (:export #:*standard-optimize-settings*
-           #:enable-sharpf-read-macro
            #:asset-path
            #:ub8
            #:ub16
@@ -26,21 +25,6 @@
 (defvar *standard-optimize-settings*
   '(optimize speed (debug 1) (space 0) (compilation-speed 0))
   "Optimize settings to use in opcode definitions (where safety 0 won't do).")
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun enable-sharpf-read-macro ()
-    "Add a read macro #f to easily declare optimization settings even in defmacro.
-   From Let Over Lambda, this is especially useful for an emulator such as clones."
-    (set-dispatch-macro-character #\# #\f
-                                  (lambda (stream sub-char numarg)
-                                    (declare (ignore stream sub-char))
-                                    (setf numarg (or numarg 3))
-                                    (unless (<= numarg 3)
-                                      (error "Invalid value for optimize declaration: ~a" numarg))
-                                    `(declare (optimize (speed ,numarg)
-                                                        (safety ,(- 3 numarg)))))))
-
-  (enable-sharpf-read-macro))
 
 (defun asset-path (namestring)
   "Compute the relative path of a static asset in the clones project."
