cefb66fd729c69c2abb37a8527e955274c3eaee7
Brit Butler <brit@kingcons.io>
2018-09-04 13:17:20 -0400

    Use the --quit option that's what it is meant for.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-sbcl --eval "(ql:quickload :clones-test)" --eval "(clones-test.helpers:run-file \"render\")" --eval "(sb-ext:quit)"
-# sbcl --eval "(asdf:test-system :clones)" --eval "(sb-ext:quit)"
+sbcl --eval "(ql:quickload :clones-test)" --eval "(clones-test.helpers:run-file \"render\")" --quit
+# sbcl --eval "(asdf:test-system :clones)" --quit
