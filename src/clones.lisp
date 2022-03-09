19d5cbcfe25b119613d874838962482ed2b7b916
Brit Butler <brit@kingcons.io>
2022-03-09 14:38:50 -0500

    Remove old travis CI config and start on docs automation.

--- src/clones.lisp
+++ src/clones.lisp
@@ -3,4 +3,21 @@
 
 (in-package :clones)
 
-;; A new world awaits...
+(defsection @clones (:title "Clones - An NES Emulator")
+  (@links section))
+
+(defsection @links (:title "Links")
+  "[![build status][badge]](https://builds.sr.ht/~kingcons/clones/commits/test.yml)
+   [badge]: https://builds.sr.ht/~kingcons/clones/commits/test.yml.svg
+
+   Here is the [git repo][repo] and
+   here is the [website][site].
+   [repo]: https://sr.ht/~kingcons/clones
+   [site]: https://clones.kingcons.io")
+
+(defun build-site ()
+  (let ((*document-normalize-packages* nil))
+    (update-asdf-system-html-docs
+     @clones :clones
+     :target-dir (asdf:system-relative-pathname :clones "site/")
+     :pages `((:objects (,clones:@clones))))))
