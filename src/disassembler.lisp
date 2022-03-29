9016ebc90e98bab716806af9dc71be14427e327c
Brit Butler <brit@kingcons.io>
2022-03-29 16:58:39 -0400

    Whoops, forgot to actually commit the disassembler.
    Definitely time to stop the stream for today.

---
+++ src/disassembler.lisp
@@ -0,0 +1,28 @@
+(mgl-pax:define-package :clones.disassembler
+  (:use :cl :alexandria :mgl-pax)
+  (:use :clones.opcodes)
+  (:import-from :clones.memory #:fetch))
+
+(in-package :clones.disassembler)
+
+(defsection @disassembler (:title "Disassembler")
+  (disasm function))
+
+(defun disasm (memory start end)
+  "Loop through MEMORY from START to END printing disassembly
+   for each instruction found in the specified range. An error
+   will be thrown if illegal instructions are present or if the
+   start index is not the beginning of a 6502 instruction."
+  (let ((table (build-opcode-table)))
+    (loop for index = start then (+ index length)
+          for byte = (fetch memory index)
+          for opcode = (aref table byte)
+          for length = (opcode-size opcode)
+          for bytes = (loop for i from index below (+ index length)
+                           collect (fetch memory i))
+          for hex = (format nil "~{~2,'0X ~}" bytes)
+          until (> index end)
+          do (format t "~4,'0X:  ~10A ;;  ~A~%"
+                     index
+                     hex
+                     (opcode-name opcode)))))
