7a2e965a5683507aa13711bee0d208f2b982b41b
Brit Butler <brit@kingcons.io>
2022-04-24 17:02:41 -0400

    Commit code for combining tile bytes.

--- src/render.lisp
+++ src/render.lisp
@@ -6,3 +6,18 @@
 
 (defsection @render (:title "The Rendering Logic")
   )
+
+(defun combine-tile-bytes (low-byte high-byte)
+  "Given the low and high byte of a pattern table tile,
+   construct the appropriate bitfield for its pixels.
+   See: https://www.nesdev.org/wiki/PPU_pattern_tables"
+  (flet ((interleave-bits (i)
+           (+ (ash (ldb (byte 1 i) high-byte) 1)
+              (ldb (byte 1 i) low-byte))))
+    (loop with result = 0
+          for i below 8
+          do (setf result (dpb
+                           (interleave-bits i)
+                           (byte 2 (* i 2))
+                           result))
+          finally (return result))))
--- test/render.lisp
+++ test/render.lisp
@@ -5,7 +5,26 @@
 (in-package :clones.test.render)
 
 (deftest test-render ()
-  )
+  (test-pattern-pixels))
+
+(deftest test-pattern-pixels ()
+  (let ((tile-low-byte #b01000001)
+        (tile-high-byte #b00000001))
+    (is (equalp (clones.render::combine-tile-bytes tile-low-byte tile-high-byte)
+                #b0001000000000011))))
+
+;; (deftest test-pattern-pixels ()
+;;   (let ((tile #(#x41 #xC2 #x44 #x48 #x10 #x20 #x40 #x80
+;;                 #x01 #x02 #x04 #x08 #x16 #x21 #x42 #x87)))
+;;     (is (equalp (tile-pixels tile)
+;;                 #(0 1 0 0 0 0 0 3
+;;                   1 1 0 0 0 0 3 0
+;;                   0 1 0 0 0 3 0 0
+;;                   0 1 0 0 3 0 0 0
+;;                   0 0 0 3 0 2 2 0
+;;                   0 0 3 0 0 0 0 2
+;;                   0 3 0 0 0 0 2 0
+;;                   3 0 0 0 0 2 2 2)))))
 
 #+nil
 (try 'test-render)
