240bdc46b5daf4e9d2bf96e2d743312b5f0bcb6a
Brit Butler <brit@kingcons.io>
2022-05-13 18:26:06 -0400

    Minor styling tweaks.

--- src/renderer.lisp
+++ src/renderer.lisp
@@ -3,6 +3,7 @@
   (:use :clones.cpu :clones.ppu)
   (:import-from :serapeum
                 #:octet
+                #:callf
                 #:~>>)
   (:import-from :alexandria
                 #:define-constant))
@@ -23,20 +24,20 @@
   Note that 1 CPU cycle is equivalent to 3 PPU cycles.")
 
 (define-constant +palette+
-    #(#(84 84 84)    #(0 30 116)    #(8 16 144)    #(48 0 136)
-      #(68 0 100)    #(92 0 48)     #(84 4 0)      #(60 24  0)
-      #(32 42  0)    #(8  58 0)     #(0 64 0)      #(0  60  0)
-      #(0  50 60)    #(0   0 0)     #(152 150 152) #(8  76 196)
-      #(48 50 236)   #(92 30 228)   #(136  20 176) #(160 20 100)
-      #(152 34 32)   #(120 60  0)   #(84  90   0)  #(40 114   0)
-      #(8 124   0)   #(0 118  40)   #(0 102 120)   #(0   0   0)
-      #(236 238 236) #(76 154 236)  #(120 124 236) #(176 98 236)
-      #(228  84 236) #(236 88 180)  #(236 106 100) #(212 136  32)
-      #(160 170   0) #(116 196  0)  #(76 208  32)  #(56 204 108)
-      #(56 180 204)  #(60  60  60)  #(236 238 236) #(168 204 236)
-      #(188 188 236) #(212 178 236) #(236 174 236) #(236 174 212)
-      #(236 180 176) #(228 196 144) #(204 210 120) #(180 222 120)
-      #(168 226 144) #(152 226 180) #(160 214 228) #(160 162 160))
+    #(#( 84  84  84)  #(  0  30 116)  #(  8  16 144)  #( 48   0 136)
+      #( 68   0 100)  #( 92   0  48)  #( 84   4   0)  #( 60  24   0)
+      #( 32  42   0)  #(  8  58   0)  #(  0  64   0)  #(  0  60   0)
+      #(  0  50  60)  #(  0   0   0)  #(152 150 152)  #(  8  76 196)
+      #( 48  50 236)  #( 92  30 228)  #(136  20 176)  #(160  20 100)
+      #(152  34  32)  #(120  60   0)  #( 84  90   0)  #( 40 114   0)
+      #(  8 124   0)  #(  0 118  40)  #(  0 102 120)  #(  0   0   0)
+      #(236 238 236)  #( 76 154 236)  #(120 124 236)  #(176  98 236)
+      #(228  84 236)  #(236  88 180)  #(236 106 100)  #(212 136  32)
+      #(160 170   0)  #(116 196   0)  #( 76 208  32)  #( 56 204 108)
+      #( 56 180 204)  #( 60  60  60)  #(236 238 236)  #(168 204 236)
+      #(188 188 236)  #(212 178 236)  #(236 174 236)  #(236 174 212)
+      #(236 180 176)  #(228 196 144)  #(204 210 120)  #(180 222 120)
+      #(168 226 144)  #(152 226 180)  #(160 214 228)  #(160 162 160))
   :documentation "The Palette information used to generate NTSC video signals
 decoded as RGB by the television set. We may later support a VGA palette format
 to specify this. See: https://www.nesdev.org/wiki/Palette#2C02"
@@ -69,8 +70,9 @@ to specify this. See: https://www.nesdev.org/wiki/Palette#2C02"
     (with-accessors ((cycles cpu-cycles)) cpu
       (when (> cycles +cycles-per-scanline+)
         (render-scanline renderer)
-        (setf scanline (mod (1+ scanline) 262))
-        (setf cycles (mod cycles 114))))))
+        (incf scanline)
+        (callf #'mod scanline 262)
+        (callf #'mod cycles 114)))))
 
 (defgeneric render-scanline (renderer)
   (:documentation "Render a scanline using RENDERER if rendering is enabled in PPUMASK."))
