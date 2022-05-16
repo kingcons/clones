a23978b2fb232215dd64d28c308f39b18a4083c0
Brit Butler <brit@kingcons.io>
2022-05-16 10:21:38 -0400

    Update notes, add CHANGE-GAME, fix the palette.

--- src/clones.lisp
+++ src/clones.lisp
@@ -7,9 +7,19 @@
 
 (in-package :clones)
 
-(defun main (cpu renderer)
+(defun change-game (cpu relative-path)
+  (let* ((path (asdf:system-relative-pathname :clones relative-path))
+         (rom (clones.mappers:load-rom path))
+         (memory (clones.cpu:cpu-memory cpu))
+         (ppu (slot-value memory 'clones.memory::ppu)))
+    (setf (slot-value memory 'clones.memory::cart) rom
+          (slot-value ppu 'clones.ppu::pattern-table) rom)))
+
+(defun main (cpu renderer &optional rom)
+  (when rom
+    (change-game cpu rom))
   (clones.cpu:reset cpu)
   (loop for count = (single-step cpu)
         do (progn
-             (now cpu)
+             ;; (now cpu)
              (sync renderer cpu))))
