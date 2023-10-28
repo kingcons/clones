1e99119e9f950f26e05edcd7b018f336f3b7543c
Brit Butler <brit@kingcons.io>
2023-10-28 15:08:38 -0400

    Add a Guix package for hacking on clones.

---
+++ guix.scm
@@ -0,0 +1,40 @@
+;;; Clones --- Common Lisp Observable Nintendo Emulator
+;;; Copyright © 2022 Brit Butler <brit@kingcons.io>
+;;;
+;;; Commentary:
+;;
+;; GNU Guix development package.  To build and install, run:
+;;
+;;   guix package -f guix.scm
+;;
+;; To use as the basis for a development environment, run:
+;;
+;;   guix shell
+;;
+;; Afterwards, we recommend starting a slynk socket for emacs to connect to:
+;;
+;;  SBCL> (clones:start-server)
+;;
+;; From there, hack away! Code:
+
+(use-modules (guix packages)
+             (guix licenses)
+             (guix git)
+             (guix build-system gnu)
+             (gnu packages)
+             (gnu packages autotools)
+             (gnu packages lisp)
+             (gnu packages lisp-xyz)
+             (gnu packages sdl))
+
+(package
+  (name "clones")
+  (version "0.0.1")
+  (source (git-checkout (url (dirname (current-filename)))))
+  (build-system gnu-build-system)
+  (native-inputs (list automake sdl2))
+  (inputs (list sbcl cl-alexandria cl-serapeum cl-mgl-pax cl-sdl2 cl-static-vectors))
+  (synopsis "Nintendo Emulator in Common Lisp")
+  (description "Clones is an NES emulator written in Common Lisp.")
+  (home-page "https://clones.kingcons.io/")
+  (license expat))
