c5209903d35b10ae3f9e335f3d71096dc9f27d7b
Brit Butler <brit@kingcons.io>
2024-06-07 13:54:00 -0400

    Use system mgl-pax now that it is up to date.

--- guix.scm
+++ guix.scm
@@ -29,67 +29,6 @@
              (gnu packages lisp-xyz)
              (gnu packages sdl))
 
-(define-public sbcl-mgl-pax
-  (let ((commit "171e93f2209147c9ab8db8ac79eeb2e911156259")
-        (revision "0"))
-    (package
-      (name "sbcl-mgl-pax")
-      (version (git-version "0.3.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/melisgl/mgl-pax")
-               (commit commit)))
-         (sha256
-          (base32 "0zdc5v4z7lysrafvn1nfpjlc8m6142zmx5axqwjwspc9rnl21ml1"))
-         (file-name (git-file-name "cl-mgl-pax" version))))
-      (build-system asdf-build-system/sbcl)
-      ;; (native-inputs
-      ;;  (list sbcl-try))
-      (inputs
-       (list sbcl-3bmd
-             sbcl-alexandria
-             sbcl-colorize
-             sbcl-hunchentoot
-             sbcl-md5
-             sbcl-named-readtables
-             sbcl-pythonic-string-reader
-             sbcl-slime-swank
-             sbcl-trivial-utf-8))
-      (arguments
-       `(#:asd-systems '("mgl-pax"
-                         "mgl-pax/bootstrap"
-                         "mgl-pax/navigate"
-                         "mgl-pax/document"
-                         "mgl-pax/transcribe"
-                         "mgl-pax/web"
-                         "mgl-pax/full")
-         ;; Tests disabled because of a circular dependency
-         ;;   try -> mgl-pax -> try
-         #:tests? #f))
-      (synopsis "Exploratory programming environment and documentation generator")
-      (description
-       "PAX provides an extremely poor man's Explorable Programming
-environment.  Narrative primarily lives in so called sections that mix markdown
-docstrings with references to functions, variables, etc, all of which should
-probably have their own docstrings.
-
-The primary focus is on making code easily explorable by using SLIME's
-@command{M-.} (@command{slime-edit-definition}).  See how to enable some
-fanciness in Emacs Integration.  Generating documentation from sections and all
-the referenced items in Markdown or HTML format is also implemented.
-
-With the simplistic tools provided, one may accomplish similar effects as with
-Literate Programming, but documentation is generated from code, not vice versa
-and there is no support for chunking yet.  Code is first, code must look
-pretty, documentation is code.")
-      (home-page "https://melisgl.github.io/mgl-pax/")
-      (license expat))))
-
-(define-public cl-mgl-pax
-  (sbcl-package->cl-source-package sbcl-mgl-pax))
-
 (package
   (name "clones")
   (version "0.0.1")
